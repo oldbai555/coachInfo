d68628c5c3dedcea02f5e7ac65febb2a6d278c00
老白 <1005777562@qq.com>
2021-01-05 15:59:52 +0800

    所有页面都已经部署，下一步加拦截器

---
+++ coach_info/src/com/neosoft/filter/CarTicketTabFilter.java
@@ -0,0 +1,27 @@
+package com.neosoft.filter;
+
+import com.neosoft.entity.Admin;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter("/carTicketTab")
+public class CarTicketTabFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpSession session = request.getSession();
+        Admin admin = (Admin) session.getAttribute("admin");
+        if (admin == null) {
+            response.sendRedirect("login.jsp");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+}
