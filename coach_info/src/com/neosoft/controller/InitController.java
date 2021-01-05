4894e41a3f42093c534a22c0dd50116496c13dae
老白 <1005777562@qq.com>
2021-01-05 17:32:10 +0800

    添加了模态框

--- coach_info/src/com/neosoft/controller/InitController.java
+++ coach_info/src/com/neosoft/controller/InitController.java
@@ -1,5 +1,7 @@
 package com.neosoft.controller;
 
+import com.neosoft.entity.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ public class InitController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        req.getSession().setAttribute("admin",new Admin("admin","123456"));
         resp.sendRedirect("login.jsp");
     }
 
